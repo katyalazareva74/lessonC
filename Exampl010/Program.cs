da1160cfc4cc4f46d4108cdb80c726ad219cf8a6
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-14 12:29:46 +0300

    Add Exampl010

---
+++ Exampl010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Поиск позиции нужного элемента
+int [] array = {31, 2, 3, 64, 5, 6, 73, 8, 91};
+int n = array.Length; // возвращает длину массива
+int find = 64, index = 0;
+while (index < n) {
+	if (array[index] == find) {
+		Console.WriteLine(index);
+		break; // если есть два одинаковых элемента
+	}
+		index++;
+	}
+

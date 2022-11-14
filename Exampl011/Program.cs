00b9e2112a5c30d8736dc3260b964ba8f8dd250d
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-14 12:34:04 +0300

    Add Exampl011

---
+++ Exampl011/Program.cs
@@ -0,0 +1,19 @@
+﻿// написание функций заполнения массива и его вывода на экран
+int [] array = new int[10];
+void FillArray (int [] collection) {
+	int length = collection.Length;
+	int index = 0;
+	while (index < length) {
+			collection[index] = new Random().Next(1, 10);
+			index++;
+}}
+
+void PrintArray (int [] col) {
+	int count = col.Length;
+	int index = 0;
+	while (index < count) {
+			Console.WriteLine(col[index]);
+			index++;
+}}
+FillArray (array);
+PrintArray (array);

23fd3ca2c536f18c891d8d6a2216bbad9f604097
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-14 12:25:39 +0300

    Add Exampl009

---
+++ Exampl009/Program.cs
@@ -0,0 +1,10 @@
+﻿// Поиск максимального элемента массива
+int [] array = {31, 2, 3, 64, 5, 116, 73, 8, 91};
+int Max (int arg1, int arg2, int arg3) {
+		int result = arg1;
+		if (arg2 > result) result = arg2;
+		if (arg3 > result) result = arg3;
+		return result;
+}        
+int max = Max (Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]),Max(array[6], array[7], array[8]));
+Console.WriteLine(max);

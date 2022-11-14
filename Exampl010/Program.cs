d76998da00229bce84ed61e47d739b1e3a9f040a
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-14 12:42:15 +0300

    Add Exampl010 с новой функцией

--- Exampl010/Program.cs
+++ Exampl010/Program.cs
@@ -1,12 +1,27 @@
 ﻿// Поиск позиции нужного элемента
 int [] array = {31, 2, 3, 64, 5, 6, 73, 8, 91};
-int n = array.Length; // возвращает длину массива
-int find = 64, index = 0;
-while (index < n) {
-	if (array[index] == find) {
-		Console.WriteLine(index);
-		break; // если есть два одинаковых элемента
-	}
+// int n = array.Length; // возвращает длину массива
+//int find = 64, index = 0;
+//while (index < n) {
+//	if (array[index] == find) {
+//		Console.WriteLine(index);
+//		break; // если есть два одинаковых элемента
+//	}
+//		index++;
+//	}
+// напишем функцию нахождения позиции нужного элемента
+int IndexOf (int [] collection, int find) {
+	int count = collection.Length;
+	int index = 0, position = -1; // обход ситуации поиска несуществующего элемента
+	while (index < count) {
+			if (collection[index] == find) {
+				position = index;
+				break;
+			}
 		index++;
 	}
+	return position;
+}
 
+int pos = IndexOf(array, 52);
+Console.WriteLine(pos);

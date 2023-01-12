07b0e5c1f156934e3c5ad66ce35782810cd3feab
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-01-12 22:26:19 +0300

    Add algoritmC#

---
+++ algoritmC/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Есть массив на входе. Нужно найти максимальную сумму  m элементов, идущих подряд.
+// Пусть будет 10 элементов, найти сумму трех элементов, идущих подряд.
+using System.Diagnostics;
+
+int size = 1000000;
+int m = 30000;
+int[] array = Enumerable.Range(1, size)
+                .Select(item => Random.Shared.Next(10))
+                .ToArray();
+//Console.WriteLine($"[{string.Join(",", array)}]");
+Stopwatch sw = new();
+sw.Start();
+int max = 0;
+// Вариант первый
+//for (int i = 0; i < array.Length - m + 1; i++)
+//{
+//  int t = 0;
+//for (int j = i; j < m + i; j++)
+//  t = t + array[j];
+//if (t > max) max = t;
+//}
+//Вариант второй
+for (int j = 0; j < m; j++)
+    max = max + array[j];
+int t = max;
+//Console.Write($"t={t}, ");
+for (int i = 1; i < array.Length - m+1; i++)
+{
+    t = t - array[i - 1] + array[i + m-1];
+   // Console.Write($"t={t}, ");
+    if (t > max) max = t;
+}
+sw.Stop();
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine(max);
---
+++ algoritmC1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Заполнить массив и подсчитать сумму его элементов
+//int n = 5;
+//int[] array = new int[n];
+//for (int i = 0; i < n; i++)
+//array[i] = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("[" + string.Join(",", array) + "]");
+//int summa = 0;
+//for (int i = 0; i < n; i++)
+//  summa += array[i];
+//Console.WriteLine(summa);
+// Заполнить таблицу умножения
+int n = Convert.ToInt32(Console.ReadLine());
+//for (int i = 0; i < n; i++)
+//{
+//  for (int j = 0; j < n; j++)
+//{
+//  Console.Write((i+1) * (j+1));
+//Console.Write("\t");
+//}
+//Console.WriteLine();
+//}
+int[,] matrix = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int k = 0; k < i; k++)
+    {
+        Console.Write($"     {matrix[i, k]}");
+    }
+    for (int j = i; j < n; j++)
+    {
+
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+        Console.Write($"     {matrix[i, j]}");
+    }
+    Console.WriteLine();
+}

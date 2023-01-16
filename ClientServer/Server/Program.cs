b5bf3a14f9f876ee7af4dc2774408151687fff7a
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-01-16 13:46:48 +0300

    Клиент-серверное взаимодействие

---
+++ ClientServer/Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Server;
+
+class Program
+{
+   static void Main(string[] args)
+    {
+        Console.WriteLine("Это наш сервер");
+        OurServer server  = new OurServer();
+    }
+}
